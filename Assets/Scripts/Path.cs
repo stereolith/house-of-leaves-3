4811e5cd8c34fe1c57ccaea3c9902315fa980de7
stereolith <lukasmoench1113@gmail.com>
2018-11-04 21:18:13 +0100

    path animation

---
+++ Assets/Scripts/Path.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEditor;
+
+[ExecuteInEditMode]
+public class Path : MonoBehaviour {
+
+    private Transform[] nodes;
+
+    private void Start()
+    {
+        nodes = GetComponentsInChildren<Transform>();
+    }
+
+    private void OnDrawGizmos() //each frame camera is moving in scene view
+    {
+        for (int i = 0; i < nodes.Length - 1; i++)
+        {
+            Handles.DrawDottedLine(nodes[i].position, nodes[i + 1].position, 3.0f);
+        }
+    }
+}
