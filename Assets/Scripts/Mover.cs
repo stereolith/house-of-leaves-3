fb6c009175ca2b5f3d3df478698d2c23cbea7dbd
stereolith <lukasmoench1113@gmail.com>
2018-11-04 22:32:26 +0100

    added interpolated movement

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -2,15 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class Mover : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Path path;
+
+    private int currentSeg; //Current segment (between nodes)
+    private float transition; //progression in segment (0-1)
+    private bool isCompleted;
+
+    private void Update()
+    {
+        if (!path)
+            return;
+        if (!isCompleted)
+            Play();
+    }
+
+    private void Play()
+    {
+        transition += Time.deltaTime * 1 / 2.5f;
+        if (transition > 1)
+        {
+            transition = 0;
+            currentSeg++;
+        }
+
+        transform.position = path.LinearPosition(currentSeg, transition);
+        transform.rotation = path.Orientation(currentSeg, transition);
+    }
 }
--- Assets/Scripts/Path.cs
+++ Assets/Scripts/Path.cs
@@ -12,6 +12,23 @@ public class Path : MonoBehaviour {
         nodes = GetComponentsInChildren<Transform>();
     }
 
+    public Vector3 LinearPosition(int seg, float ratio)
+    {
+        Vector3 p1 = nodes[seg].position;
+        Vector3 p2 = nodes[seg + 1].position;
+
+        return Vector3.Lerp(p1 ,p2 ,ratio);
+    }
+
+    public Quaternion Orientation(int seg, float ratio)
+    {
+        Quaternion q1 = nodes[seg].rotation;
+        Quaternion q2 = nodes[seg + 1].rotation;
+
+        return Quaternion.Lerp(q1, q2, ratio);
+    }
+    //public Vector3 CatmullPosition(int seg, float ratio);
+
     private void OnDrawGizmos() //each frame camera is moving in scene view
     {
         for (int i = 0; i < nodes.Length - 1; i++)
