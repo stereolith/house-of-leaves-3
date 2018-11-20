3431f1778d1d1572d3dcdd7657ceacd7f9099a91
stereolith <lukasmoench1113@gmail.com>
2018-11-21 00:04:53 +0100

    add missing untracked files

---
+++ Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour {
+
+    public float mouseSensitivity = 100;
+
+    private float pitch;
+    private float yaw;
+
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    private void Update()
+    {
+        pitch += -Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        yaw += Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+
+        pitch = Mathf.Clamp(pitch, -90f, 90f);
+
+        while (yaw < 0f)
+        {
+            yaw += 360f;
+        }
+        while (yaw >= 360f)
+        {
+            yaw -= 360f;
+        }
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0f);
+    }
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMove : MonoBehaviour {
+
+    public Image progressCircle;
+
+    public float timeToSelect = 1000f;
+    public float regainHitTimeout = 50f;
+    private float regainHitCountdown;
+    private bool selecting;
+    private float timeOnTarget = 0f;
+
+    void Awake()
+    {
+        regainHitCountdown = regainHitTimeout;
+    }
+
+    void Update()
+    {
+        RaycastHit hit;
+        Ray lookRay = new Ray(transform.position, transform.forward);
+
+        if(Physics.Raycast(lookRay, out hit))
+        {
+            if(hit.collider.tag == "moveTarget")
+            {
+                selecting = true;
+                regainHitCountdown = regainHitTimeout;
+                timeOnTarget += Time.deltaTime;
+                UpdateProgressCircle();
+                if (timeOnTarget >= timeToSelect)
+                {
+                    Debug.Log("Succesfully selected!!");
+                    selecting = false;
+                    timeOnTarget = 0f;
+                }
+            } else if (selecting)
+            {
+                regainHitCountdown -= Time.deltaTime;
+                if (regainHitCountdown <= 0)
+                {
+                    selecting = false;
+                    regainHitCountdown = regainHitTimeout;
+                    timeOnTarget = 0f;
+                    Debug.Log("sorry, Timeout!");
+                }
+            }
+        }
+    }
+
+    void UpdateProgressCircle()
+    {
+        progressCircle.fillAmount = timeOnTarget / timeToSelect;
+    }
+}
