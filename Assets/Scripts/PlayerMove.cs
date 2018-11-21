3b51b70889b0f54bcbc746134c187d27c3ba9c53
stereolith <lukasmoench1113@gmail.com>
2018-11-21 11:56:58 +0100

    add movement

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -9,9 +9,12 @@ public class PlayerMove : MonoBehaviour {
 
     public float timeToSelect = 1000f;
     public float regainHitTimeout = 50f;
+    public int walkSpeed = 5;
     private float regainHitCountdown;
     private bool selecting;
     private float timeOnTarget = 0f;
+    private Transform target = null;
+    private bool isWalking = false;
 
     void Awake()
     {
@@ -20,12 +23,25 @@ public class PlayerMove : MonoBehaviour {
 
     void Update()
     {
+        if(!isWalking)
+        {
+            RaycastSelect();
+        }
+        else
+        {
+            WalkTo();
+        }
+        
+    }
+
+    void RaycastSelect()
+    {
         RaycastHit hit;
         Ray lookRay = new Ray(transform.position, transform.forward);
 
-        if(Physics.Raycast(lookRay, out hit))
+        if (Physics.Raycast(lookRay, out hit))
         {
-            if(hit.collider.tag == "moveTarget")
+            if (hit.collider.tag == "moveTarget")
             {
                 selecting = true;
                 regainHitCountdown = regainHitTimeout;
@@ -34,10 +50,13 @@ public class PlayerMove : MonoBehaviour {
                 if (timeOnTarget >= timeToSelect)
                 {
                     Debug.Log("Succesfully selected!!");
+                    target = hit.transform;
+                    isWalking = true;
                     selecting = false;
                     timeOnTarget = 0f;
                 }
-            } else if (selecting)
+            }
+            else if (selecting)
             {
                 regainHitCountdown -= Time.deltaTime;
                 if (regainHitCountdown <= 0)
@@ -45,6 +64,7 @@ public class PlayerMove : MonoBehaviour {
                     selecting = false;
                     regainHitCountdown = regainHitTimeout;
                     timeOnTarget = 0f;
+                    UpdateProgressCircle();
                     Debug.Log("sorry, Timeout!");
                 }
             }
@@ -54,5 +74,23 @@ public class PlayerMove : MonoBehaviour {
     void UpdateProgressCircle()
     {
         progressCircle.fillAmount = timeOnTarget / timeToSelect;
+    }
+
+    void WalkTo()
+    {
+        float moveStep = walkSpeed * Time.deltaTime;
+        Vector3 targetPos = target.position;
+        targetPos.y = transform.parent.position.y;
+        transform.parent.position = Vector3.MoveTowards(transform.parent.position, targetPos, moveStep);
+
+    }
+
+
+    void lookAtSmooth(Transform rotTarget) //ToDo: rotation muss mauseingabe (PlayerLook.cs) überschreiben
+    {
+        Vector3 targetDir = rotTarget.position - transform.position;
+        targetDir.y = 0f;
+
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(targetDir), Time.deltaTime);
     }
 }
