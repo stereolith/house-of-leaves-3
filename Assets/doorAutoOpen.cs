2f9da943806ffcdbff6ff2e448e54d2e5ac6a6b1
kimrobine <kimrobineroland@web.de>
2018-12-20 18:00:40 +0100

    add door open on collider

--- Assets/doorAutoOpen.cs
+++ Assets/doorAutoOpen.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 public class doorAutoOpen : MonoBehaviour {
 
     public Transform playerTransform;
+    private Collider boxCollider;
+
+    private Animator anim;
 
 	// Use this for initialization
 	void Start () {
-
-		
-	}
+        boxCollider = GetComponentsInChildren<Collider>()[0];
+        anim = GetComponent<Animator>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (boxCollider.bounds.Contains(playerTransform.position))
+        {
+            anim.SetTrigger("open_door");
+        }
+    }
 }
