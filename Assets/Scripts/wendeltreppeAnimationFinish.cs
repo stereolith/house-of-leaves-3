645e822008089b3f22990365d373962b8146d4f2
stereolith <lukasmoench1113@gmail.com>
2019-01-30 13:53:09 +0100

    fix activation of treppewalktarget

---
+++ Assets/Scripts/wendeltreppeAnimationFinish.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wendeltreppeAnimationFinish : MonoBehaviour {
+
+    private GameObject walkTarget;
+
+    void Awake()
+    {
+        walkTarget = GameObject.Find("walkTarget_walkPath");
+        walkTarget.SetActive(false);
+    }
+
+	public void handleANimationFinish()
+    {
+        Debug.Log(walkTarget);
+        walkTarget.SetActive(true);
+    }
+}
